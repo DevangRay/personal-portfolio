6f801311ebd363eeef48550c679629304a88b40a
Devang Ray <devangray624@gmail.com>
2026-03-17 23:29:43 -0400

    fixed hero image styling

--- app/components/HeroTitle.tsx
+++ app/components/HeroTitle.tsx
@@ -4,12 +4,12 @@ export default function HeroTitle() {
             <h1 className="text-6xl sm:text-8xl font-bold">
                 Devang Ray
             </h1>
-            <h2>
+            <h2 className="text-4xl sm:text-6xl font-bold">
                 Full Stack Developer
             </h2>
-            <h3>
+            <h3 className="text-2xl sm:text-4xl">
                 Developer based around Washington DC, USA.
             </h3>
-        </>
+        </div>
     )
 }
