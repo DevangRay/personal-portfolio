cddf26bfe6e519f593b5ecd78874dd9124332fe2
Devang Ray <devangray624@gmail.com>
2026-03-17 23:29:20 -0400

    fixed sizing of homepage

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,9 @@ import HeroTitle from "@/app/components/HeroTitle";
 export default function Home() {
   return (
     <div className="min-h-screen font-sans">
-      <main className="flex w-full max-w-6xl flex-col items-center py-8 px-16 sm:items-start mx-auto">
+      <main className="flex w-full max-w-5xl flex-col items-center py-8 px-16 sm:items-start mx-auto">
         <HeroTitle />
+        
         <div className="min-h-[100vh]">
           This text goes long
         </div>
