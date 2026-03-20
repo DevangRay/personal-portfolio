2c38cc5ee2097620c0f1bd0dcfacffe5f0d1b2bc
Devang Ray <devangray624@gmail.com>
2026-03-19 21:56:50 -0400

    added link to profile photo

--- app/components/navbar/ProfilePhoto.tsx
+++ app/components/navbar/ProfilePhoto.tsx
@@ -1,16 +1,19 @@
 import Image from "next/image";
+import Link from "next/link"
 
 export default function ProfilePhoto() {
   return (
     <div className="relative w-[60px] h-[60px] rounded-full overflow-hidden shadow-xl">
-      <Image
-        src="/images/icon-circle.jpg"
-        alt="Devang Ray"
-        fill
-        className="object-cover object-top"
-        sizes="192px"
-        priority
-      />
+      <Link href={"#home"}>
+        <Image
+          src="/images/icon-circle.jpg"
+          alt="Devang Ray"
+          fill
+          className="object-cover object-top"
+          sizes="192px"
+          priority
+        />
+      </Link>
     </div>
   );
 }
