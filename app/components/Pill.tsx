b3c25cba084a699b78e92536a26e7d7ac5d85d6c
Devang Ray <devangray624@gmail.com>
2026-03-23 21:31:32 -0400

    standardized capitalization of Pill copmonent

--- app/components/Pill.tsx
+++ app/components/Pill.tsx
@@ -1,12 +1,20 @@
 import { cn } from "@/lib/utils";
 
-export default function Pill({skill, className}: {skill: string, className?: string}) {
+export default function Pill({ skill, className }: { skill: string, className?: string }) {
+    function capitalizeString(string: string) {
+        const capitalized_words = string.split(" ").map((word) => {
+            return word.charAt(0).toLocaleUpperCase() + word.slice(1)
+        })
+
+        return capitalized_words.join(" ");
+    }
+
     return (
         <>
             <span
                 className={cn("text-xs px-2 py-0.5 rounded-full bg-secondary text-secondary-foreground border border-border cursor-default", className)}
             >
-                {skill}
+                {capitalizeString(skill)}
             </span>
         </>
     )
