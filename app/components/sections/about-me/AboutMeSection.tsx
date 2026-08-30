aafe43bb20b0807372e389e35456162ac182fc11
Devang Ray <devangray624@gmail.com>
2026-08-29 21:51:37 -0400

    fixed About Me wording

--- app/components/sections/about-me/AboutMeSection.tsx
+++ app/components/sections/about-me/AboutMeSection.tsx
@@ -11,10 +11,10 @@ export default function AboutMeSection() {
 
                 <div className="flex flex-col gap-4 text-(--muted-foreground)">
                     <p>
-                        Hello! I'm Devang Ray, a full-stack developer passionate about building user-friendly products that solve problems and create genuine value rather thn simply shipping solutions.
+                        Hello! I'm Devang Ray, a full-stack developer passionate about building user-friendly products that solve problems and create genuine value rather than simply shipping solutions.
                     </p>
                     <p>
-                        Over my career I've grown to thrive in both fast-paced environments where experimentation is valued, and in more mature and complex environments where developing resilient, available situations are crucial.
+                        Over my career I've grown to thrive in both fast-paced environments where experimentation is valued, and in more mature and complex environments where developing resilient, highly-available situations are crucial.
                     </p>
                     <p>
                         Separate from Software Development, I love travelling, baking, and getting outside as often as I can!
